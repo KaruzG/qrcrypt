740584cc505a56c3a0a23cc4eb5b3dcd52e2cf76
KaruzG <josemiguelcpcs@gmail.com>
2025-04-30 14:20:31 +0200

    feat(base): Main files and configurations

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      project: './tsconfig.json'
+    },
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking'
+    ],
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-floating-promises': 'error'
+    },
+    ignorePatterns: ['dist/', 'node_modules/']
+  };
+  
---
+++ README.md
@@ -0,0 +1,11 @@
+## 📚 QRCrypt
+
+QRCrypt is a TypeScript library that allows you to generate QR codes with encrypted data. It's perfect for securely storing sensitive information in a scannable format.
+
+## 🚀 Installation
+
+To install QRCrypt in your project, run:
+
+```bash
+npm install qrcrypt
+```

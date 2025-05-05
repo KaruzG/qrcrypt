aa39f6147fb7117757c626ef94f00475a002443b
KaruzG <josemiguelcpcs@gmail.com>
2025-05-05 17:48:45 +0200

    feat(encrypt): implement encrypt function for data encryption

---
+++ src/encrypt.ts
@@ -0,0 +1,30 @@
+import {toBase64, generateKey} from "./utils/utils"
+
+type encryptParams = {
+    data: string,
+    password: string,
+}
+
+type encryptReturn = {
+    initializationVector: string,
+    salt: string,
+    encryptedData: string
+}
+
+export async function encrypt(params: encryptParams):Promise<encryptReturn> {
+    const {data, password} = params
+    const encodedData = new TextEncoder().encode(data)
+    const initializationVector = crypto.getRandomValues(new Uint8Array(12))
+    const salt = crypto.getRandomValues(new Uint8Array(16))
+    const key = await generateKey({password: password, salt: salt})
+
+    const encryptedData = await crypto.subtle.encrypt ({
+        name: "AES-GCM",
+        iv: initializationVector
+    },key, encodedData)
+    return {
+        initializationVector: toBase64(initializationVector),
+        salt: toBase64(salt),
+        encryptedData: toBase64(encryptedData)
+    }
+}
